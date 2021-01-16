3e83b3188ab87902339849d3ea4ca86620e7c1cd
Ayman <ayman.patel97@gmail.com>
2021-01-16 15:49:37 +0530

    Python script

---
+++ algolia.py
@@ -0,0 +1,42 @@
+import os
+import csv
+
+from algoliasearch.search_client import SearchClient
+
+# add conditional for github workflow 
+
+
+from jproperties import Properties
+configs = Properties()
+with open('keys.properties', 'rb') as config_file:
+    configs.load(config_file)
+
+
+#  Application ID and API Key
+#client = SearchClient.create(configs.get("ALGOLIAapplicationid").data, configs.get("ALGOLIAapikeysecret").data) #local
+
+client = SearchClient.create("T81G59BI39", str(os.environ.get("KEY")))
+#client = SearchClient.create('T81G59BI39', '<>')
+
+
+index = client.init_index("my-notes")
+
+
+def add_records(filename: str):
+
+    with open(filename, newline="") as f:
+        csv_r = list(csv.DictReader(f, delimiter=";"))
+        len_idx = index.search("")["nbHits"]
+
+        if len(csv_r) > len_idx:
+            index.save_objects(
+                csv_r[len_idx:], {"autoGenerateObjectIDIfNotExist": "true"}
+            )
+            print(f"{len(csv_r[len_idx:])} new records added.")
+            return
+
+    print("Nothing new.")
+
+
+if __name__ == "__main__":
+    add_records("projects.csv")
