07cd4176722757ec6eddd34dc667bc88ce37fa7e
Ayman <ayman.patel97@gmail.com>
2021-03-15 18:12:39 +0530

    Add tag support

--- algolia.py
+++ algolia.py
@@ -3,8 +3,18 @@ import csv
 
 from algoliasearch.search_client import SearchClient
 
+# add conditional for github workflow 
+#  Application ID and API Key
 
-client = SearchClient.create("T81G59BI39", str(os.environ.get("KEY")))
+
+#from jproperties import Properties
+#configs = Properties()
+#with open('keys.properties', 'rb') as config_file:
+#    configs.load(config_file)
+#client = SearchClient.create(configs.get("ALGOLIAapplicationid").data, configs.get("ALGOLIAapikeysecret").data) #local
+
+client = SearchClient.create("T81G59BI39", "af12ec85466e0e85bc8cc01bf527cfde")
+
 index = client.init_index("my-notes")
 
 
@@ -12,14 +22,18 @@ def add_records(filename: str):
 
     with open(filename, newline="") as f:
         csv_r = list(csv.DictReader(f, delimiter=";"))
-        len_idx = index.search("")["nbHits"]
 
-        if len(csv_r) > len_idx:
-            index.save_objects(
-                csv_r[len_idx:], {"autoGenerateObjectIDIfNotExist": "true"}
-            )
-            print(f"{len(csv_r[len_idx:])} new records added.")
-            return
+        # Bug: This checks # of rows and not change in indices
+        try: 
+            len_idx = index.search("")["nbHits"]
+            if len(csv_r) > len_idx:
+                index.save_objects(
+                    csv_r[len_idx:], {"autoGenerateObjectIDIfNotExist": "true"}
+                )
+                print(f"{len(csv_r[len_idx:])} new records added.")
+                return
+        except:
+            print("Exception")   
 
     print("Nothing new.")
 
